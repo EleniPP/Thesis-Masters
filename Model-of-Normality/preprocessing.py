c5e264be1c5f9f1fe2c519c96e1f1518ebd75027
EleniPP <eleni1997pp@gmail.com>
2024-03-22 20:38:12 +0200

    align-fail

--- Model-of-Normality/preprocessing.py
+++ Model-of-Normality/preprocessing.py
@@ -5,7 +5,8 @@ import numpy as np
 import wave
 import random
 import librosa
-
+from scipy.spatial.distance import euclidean
+from fastdtw import fastdtw
 
 
 def extract_zip():
@@ -25,9 +26,36 @@ def preprocessing(audio):
     shifted_audio = librosa.effects.pitch_shift(perturbed_audio, sr=sr, n_steps=semitones)
     return shifted_audio
 
+# I dont like it there is a big print and i dont understand what is happening
+def align_and_dividing(preprocessed_audio,visual):
+    # Align sequences using DTW
+    # there is a weird print there for some reason
+    distance, path = fastdtw(preprocessed_audio, visual, dist=euclidean)
+
+    # Extract aligned segments
+    alignment_indices = [pair[1] for pair in path]
+    segment_duration = 3.5  # Segment duration in seconds
+    sampling_rate = 44100  # Example sampling rate for audio data (adjust as needed)
+    segment_indices = [int(idx * sampling_rate) for idx in alignment_indices]
+
+    # Extract segments of aligned data
+    aligned_segments = []
+    for idx in segment_indices:
+        segment_end = idx + int(segment_duration * sampling_rate)
+        aligned_segment = preprocessed_audio[idx:segment_end], visual[path[idx]]
+        aligned_segments.append(aligned_segment)
+    return aligned_segments
+
+
 # Read file
 file = "C:/Users/eleni/Data/303_P/303_AUDIO.wav"
-visual = "C:/Users/eleni/Data/303_P/303_CLNF_AUs.txt"
+file_visual = "C:/Users/eleni/Data/303_P/303_CLNF_AUs.txt"
 audio, sr = librosa.load(file, sr=None) 
 
+# , errors='ignore'
+file_visual = open(file_visual, "r")
+visual = file_visual.read()
+
 preprocessed_audio = preprocessing(audio)
+
+aligned_segments = align_and_dividing(preprocessed_audio, visual)
